567ae099bf61de93a0e9b0209100e94358d2af42
Bart van den Burg <bart@bartvandenburg.nl>
2021-11-02 17:29:48 +0100

    bug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import {collection, collectionData, Firestore, CollectionReference} from '@angular/fire/firestore';
+import {AngularFirestore} from '@angular/fire/compat/firestore';
+import {Observable} from 'rxjs';
+
+type MyType = { title: string; };
+type WithId<T> = T & { id: string; };
 
 @Component({
   selector: 'app-root',
@@ -7,4 +13,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'fire-bug';
+
+  constructor(private firestore: Firestore, private compatFirestore: AngularFirestore) {}
+
+  a(): Observable<WithId<MyType>[]> {
+
+    const coll = collection(this.firestore, 'test') as CollectionReference<MyType>;
+
+    // not ok
+    return collectionData(coll, {idField: 'id'});
+
+  }
+
+  b(): Observable<WithId<MyType>[]> {
+
+    // ok
+    return this.compatFirestore.collection<MyType>('test').valueChanges({idField: 'id'});
+
+  }
+
 }
